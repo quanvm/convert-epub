9c25b2351f7b7d81897f3c89f451636326402df4
quan vo <quan.vo@go1.com>
2023-05-17 14:49:15 +0700

    Support download by config.txt

--- README.md
+++ README.md
@@ -9,17 +9,30 @@ brew install php@7.4
 composer install
 ```
 
-## Using
+## Download single
 - -n: short name, retrieved from url: https://bachngocsach.com.vn/reader/ban-long
 - -r: recipe name, allowed values: bachngocsach, truyendichz
 
 ```shell
 ./download.sh -n ban-long
-``` 
+```
 
 ```shell
 ./download.sh -n truyen-bach-luyen-thanh-than -r truyendichz
-``` 
+```
+
+## Download by config file
+- Define a list in config.txt
+
+```shell
+-n ban-long
+-n truyen-bach-luyen-thanh-than -r truyendichz
+```
+
+- Execute download.sh
+```shell
+./download.sh
+```
 
 ## Recipe
 - bachngocsach: https://bachngocsach.com.vn/reader/
--- download.sh
+++ download.sh
@@ -1,37 +1,7 @@
-##!/bin/sh
+#!/bin/bash
 
-recipe="bachngocsach"
+file="config.txt"
 
-while getopts n:r: flag
-do
-    case "${flag}" in
-        n) name=${OPTARG};;
-        r) recipe=${OPTARG};;
-    esac
-done
-
-if [ ! -d "data/$name" ]; then
- mkdir -p "data/$name"
-fi
-path="data/$name"
-
-pages=`php crawler/DownloadPage.php $name $recipe`
-i=0
-while [ $i -le $pages ]
-do
-  printf "Downloading: $i\r"
-  page=$[1099 + $i*100]
-  if [ ! -f "$path/$page.html" ]; then
-   php download.php $name $i $recipe
-   sleep 1
-  fi
-  i=$[$i+1]
-done
-
-title=`php crawler/DownloadTitle.php $name $recipe`
-
-path="data/$name"
-folder="$(pwd)/$path"
-cover=`find $path -type f -name "logo.*"`
-
-/Applications/calibre.app/Contents/MacOS/ebook-convert "recipe/$recipe.recipe" "data/$name.epub" --title-sort $folder --title "$title" --cover "$cover"
+while IFS= read -r line; do
+  ./download_single.sh $line
+done < "$file"
